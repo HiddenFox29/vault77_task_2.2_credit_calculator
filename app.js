75778f14b16bbace60dd397e9090585a13bdd661
Aleksandr Alekseev <hiddenfox29@yandex.ru>
2019-09-30 21:00:14 +0600

    my version calculate

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 
 // ваш код
-
+let sum = prompt('Введите сумму займа в рублях ');
+let time = prompt('Введите время займа');
+let interest = 10;
+let daily = (sum/time * interest).toFixed(2);
+let overflow = ((time*daily)-sum).toFixed(2);
 // подставте значениея
-document.getElementById('sum').innerHTML = '0';
-document.getElementById('time').innerHTML = '0';
-document.getElementById('interest').innerHTML = '0';
-document.getElementById('daily').innerHTML = '0';
-document.getElementById('overflow').innerHTML = '0';
+document.getElementById('sum').innerHTML = sum + ' руб';
+document.getElementById('time').innerHTML = time + ' дней';
+document.getElementById('interest').innerHTML = interest + ' %';
+document.getElementById('daily').innerHTML = daily + ' руб';
+document.getElementById('overflow').innerHTML = overflow + ' руб';
